1dccdc0c8c60cb45d369046cd763b81b494f6a81
Dariusz Trawicki <dt.dartit@gmail.com>
2025-07-17 12:57:46 +0200

    chore: update blade code examples

--- programming-skills/laravel-blade-tailwind/drop-navbar.blade.php
+++ programming-skills/laravel-blade-tailwind/drop-navbar.blade.php
@@ -74,15 +74,13 @@
                 </li>
                 <li>
                     <!-- Dropdown menu -->
-                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" data-dropdown-trigger="hover"md-se
-                            class="flex items-center justify-between w-full py-2 pl-3
-                                pr-4 rounded hover:text-gold active:bg-normal-50 focus:bg-normal-50 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:white
-                                md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white
-                                dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent
-                              block py-2 pl-3 pr-4
-                              md:p-0 dark:text-white md:dark:hover:text-WHITE dark:hover:bg-gray-700
-                              font-display
-                            dark:hover:text-white md:dark:hover:bg-transparent">
+                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" data-dropdown-trigger="hover"
+                        class="flex items-center justify-between w-full py-2 pl-3 pr-4 rounded
+                            hover:text-gold active:bg-normal-50 focus:bg-normal-50 hover:bg-gray-100
+                            md:hover:bg-transparent md:border-0 md:p-0 md:w-auto
+                            dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white
+                            dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent
+                            font-display">
                         <span class="link link-underline link-underline-black">OFERTA</span>
                         <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
