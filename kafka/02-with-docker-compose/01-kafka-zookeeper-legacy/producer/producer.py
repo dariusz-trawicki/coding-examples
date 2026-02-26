d498633f044f15f4d69f282a10dcc292fdcecac6
Dariusz Trawicki <dt.dartit@gmail.com>
2026-02-26 11:16:51 +0100

    feat:02-kafka-kraft-demo

--- kafka/02-with-docker-compose/01-kafka-zookeeper-legacy/producer/producer.py
+++ kafka/02-with-docker-compose/01-kafka-zookeeper-legacy/producer/producer.py
@@ -8,6 +8,6 @@ users = ["Anna", "John", "Tom", "Sophie"]
 while True:
     data = {"user": random.choice(users), "action": "click"}
     producer.produce("user_clicks", value=json.dumps(data))
-    print("Wysłano:", data)
+    print("Sent:", data)
     producer.flush()
     time.sleep(1)
